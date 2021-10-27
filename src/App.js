4935cdf62b3bcee95f5a9e985dddd4e93dec1286
Julio <julio@ctrlsun.com>
2021-10-27 05:19:29 -0500

    Chapter 2, lesson 1.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
-import { useInput } from "react-hookedup"
+import React from 'react';
+import ReactDOM from "react-dom";
+
+let values = []
+let currentHook = 0
+
+function useState(initialState) {
+  if (typeof values[currentHook] === "undefined") values[currentHook] = initialState
+
+  let hookIndex = currentHook
+
+  function setState(nextValue) {
+    values[hookIndex] = nextValue
+    ReactDOM.render(<MyName />, document.getElementById("root"))
+  }
+
+  return [values[currentHook++], setState]
+}
+
 
 const MyName = () => {
-  const { value, onChange } = useInput("")
+  currentHook = 0
+  const [name, setName] = useState("")
+  const [lastName, setLastName] = useState("")
+
+  const handleChange = evt => {
+    setName(evt.target.value)
+  }
+
+  const handleLastNameChange = evt => setLastName(evt.target.value)
 
   return (
     <div>
-      <h1>My name is: {value}</h1>
-      <input type="text" value={value} onChange={onChange}/>
+      <h1>My name is: {name} {lastName}</h1>
+      <input type="text" value={name} onChange={handleChange} />
+      <input type="text" value={lastName} onChange={handleLastNameChange} />
     </div>
   );
 };
